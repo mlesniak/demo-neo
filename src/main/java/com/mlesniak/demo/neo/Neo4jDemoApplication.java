42a43be83e62ffd504e66ea8ccd502e29a04d37e
Michael Lesniak <mlesniak@micromata.de>
2016-11-17 22:08:11 +0100

    Initial commit

---
+++ src/main/java/com/mlesniak/demo/neo/Neo4jDemoApplication.java
@@ -0,0 +1,12 @@
+package com.mlesniak.demo.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Neo4jDemoApplication.class, args);
+  }
+}
